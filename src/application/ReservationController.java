1213e09886bde646b9cb10138674b9d115278aaa
sahli-abderazak <sahliabderazak53@gmail.com>
2024-05-12 03:04:33 +0100

    proj 2

---
+++ src/application/ReservationController.java
@@ -0,0 +1,14 @@
+package application;
+
+public class ReservationController {
+    private int selectedCarId;
+
+    // Méthode pour initialiser les données avec l'ID de la voiture sélectionnée
+    public void initData(int id) {
+        this.selectedCarId = id;
+       
+    }
+}
+
+
+
